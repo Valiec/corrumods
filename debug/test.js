c5984f6891e7fe40e18607661230c9b7cc5116e5
Valiec <valiec2019@gmail.com>
2025-02-02 11:38:06 -0500

    Update test.js

--- debug/test.js
+++ debug/test.js
@@ -3,4 +3,11 @@ function onload_debug() {
   swup.loadPage({url: "https://corru.observer/local/ocean/ship/interview?force"}); //debug, should send you to the interview instead of every other page
 }
 
+let doRender = swup.renderPage;
+swup.renderPage = function(s, p, o) { console.log("hi I hooked into this"); doRender(s, p, o); }
+
+//addEventListener("animation:out:end", function(a){ console.log("foo") })
+
 onload_debug();
+
+
