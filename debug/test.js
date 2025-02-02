e5d0f45543f2a25f5da5c9ce584710ac51773efd
Valiec <valiec2019@gmail.com>
2025-02-02 10:38:06 -0500

    Update test.js

--- debug/test.js
+++ debug/test.js
@@ -3,13 +3,4 @@ function onload_debug() {
   swup.loadPage({url: "https://corru.observer/local/ocean/ship/interview?force"}); //debug, should send you to the interview instead of every other page
 }
 
-if(document.readyState == 'complete')
-{
-  console.log("ready, calling onload_debug()");
-  onload_debug();
-}
-else
-{
-  console.log("not ready");
-  console.log(document.readyState);
-}
+addEventListener("load", onload_debug);
