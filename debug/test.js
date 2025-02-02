b7b290756626e370fd9a52944e153b658d18a968
Valiec <valiec2019@gmail.com>
2025-02-02 15:20:25 -0500

    Update test.js
    get the URL right

--- debug/test.js
+++ debug/test.js
@@ -102,6 +102,7 @@ function overridePageIfNeeded(pageRec)
         }
         request.send();
     }
+    request.responseURL = pageRec.responseURL; //fake the URL
     pageRec = swup.getPageData(request);
   }
   return pageRec;
