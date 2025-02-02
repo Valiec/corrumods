57604dcd779da8e9bde6e2e25c9dd6b7b8acdc9f
Valiec <valiec2019@gmail.com>
2025-02-02 10:37:23 -0500

    Update test.js

--- debug/test.js
+++ debug/test.js
@@ -1,5 +1,6 @@
 function onload_debug() {
   console.log('loaded');
+  swup.loadPage({url: "https://corru.observer/local/ocean/ship/interview?force"}); //debug, should send you to the interview instead of every other page
 }
 
 if(document.readyState == 'complete')
